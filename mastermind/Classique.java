fe2023868906e829e67af9c9d3491ba6ae398355
Saif Zouaoui Mahjoub <saif.zouaoui-mahjoub@etu.unistra.fr>
2023-12-08 12:00:21 +0100

    correction mode ,iùeroqie

--- mastermind/Classique.java
+++ mastermind/Classique.java
@@ -3,8 +3,8 @@ package mastermind;
 public class Classique implements ModeJeu {
 
     public  void afficherIndices(LigneIndice ligne, String titre) {
-        System.out.print(titre + " : " +ligne.getIndices()[2] + " mal placer");
-        for (int i = 0; i <ligne.getIndices()[0]; i++) {
+        System.out.print(titre + " : ");
+           for (int i = 0; i <ligne.getIndices()[0]; i++) {
             System.out.print(Indice.BIEN_PLACE+ " ");
         }
         for (int i = 0; i <ligne.getIndices()[1]; i++) {
--- mastermind/Numerique.java
+++ mastermind/Numerique.java
@@ -4,7 +4,7 @@ public class Numerique implements ModeJeu{
 
 
         public  void afficherIndices(LigneIndice ligne, String titre) {
-            System.out.print(titre + " : "+ligne.getIndices()[0]+" bien placer  et"+ ligne.getIndices()[2] + " mal placer");
+            System.out.print(titre + " : "+ligne.getIndices()[0]+" bien placer  et"+ ligne.getIndices()[1] + " mal placer");
 
             System.out.println(); // Pour passer à la ligne après l'affichage des couleurs
         }
