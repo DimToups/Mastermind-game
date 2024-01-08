6664082fb6006170e65da900d5e770441a014b07
Dim_Toups <dimitri.toupense@etu.unistra.fr>
2024-01-08 15:32:23 +0100

    Mise à jour du DCC et correction d'erreurs mineures

--- src/model/observers/ObservateurUI.java
+++ src/model/observers/ObservateurUI.java
@@ -1,8 +1,5 @@
 package src.model.observers;
 
-import src.model.Combinaison;
-
 public interface ObservateurUI extends ObservateurPartie, ObservateurManche, ObservateurTentative{
 
-    void affichageCombinaison(Combinaison combinaison);
 }
