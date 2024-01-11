a829f204d74092d2309ba682654bd6914c943afe
Dim_Toups <dimitri.toupense@etu.unistra.fr>
2024-01-11 22:04:34 +0100

    Ajout de deux arguments de lancement pour l'interface utilisateur

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,20 @@ public class Main {
      * Lance le jeu mastermind
      *
      * @param args Les arguments de lancement du programme
+     *             -f ou --fenetre : affichage avec une fenêtre
+     *             -c ou --console : affichage avec la console
      */
     public static void main(String[] args) {
         GestionnaireJeu jeu = new GestionnaireJeu();
         jeu.setPartie(new Partie(jeu));
-        jeu.setUI(new AffichageConsole(jeu));
+
+        // Définition de l'interface utilisateur à utiliser
+        if(args.length == 0)
+            jeu.setUI(new AffichageFenetre(jeu));
+        else if(args[0].equals("-c") || args[0].equals("--console"))
+            jeu.setUI(new AffichageConsole(jeu));
+        else if(args[0].equals("-f") || args[0].equals("--fenetre"))
+            jeu.setUI(new AffichageFenetre(jeu));
         jeu.initialiserPartie();
     }
 }
